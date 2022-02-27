7af47106b482bf8f47fada420a353a9adff8e4c7
mynus01 <matheus.sandri@premiersoft.net>
2022-02-27 12:47:24 -0300

    test: refactor tests using bdd (I hope CI still detects the test classes)

---
+++ TextInputForm/src/test/java/com/mynus01/textinputform/cucumber/steps/FieldTypeSteps.kt
@@ -0,0 +1,36 @@
+package com.mynus01.textinputform.cucumber.steps
+
+import com.mynus01.textinputform.enums.FieldType
+import com.mynus01.textinputform.util.isValidCNPJ
+import com.mynus01.textinputform.util.isValidCPF
+import com.mynus01.textinputform.util.isValidEmail
+import io.cucumber.java.en.Given
+import io.cucumber.java.en.Then
+import io.cucumber.java.en.When
+import org.junit.Assert.assertEquals
+
+class FieldTypeSteps {
+    private lateinit var fieldType: FieldType
+    private var result: Boolean = false
+
+    @Given("FieldType is {string}")
+    fun field_type_is(type: String) {
+        fieldType =  FieldType.valueOf(type)
+    }
+
+    @When("{string} is inserted")
+    fun value_is_inserted(value: String) {
+        result = when(fieldType) {
+            FieldType.CPF -> value.isValidCPF()
+            FieldType.CNPJ -> value.isValidCNPJ()
+            FieldType.EMAIL -> value.isValidEmail()
+            else -> true
+        }
+    }
+
+    @Then("it should return {string}")
+    fun it_should_return(value: String) {
+        val expected = value == "true"
+        assertEquals(expected, result)
+    }
+}
--- app/src/test/java/com/mynus01/textinputformexample/UnitTestsSuite.kt
+++
@@ -1,8 +0,0 @@
-package com.mynus01.textinputformexample
-
-import org.junit.runner.RunWith
-import org.junit.runners.Suite
-
-@RunWith(Suite::class)
-@Suite.SuiteClasses(ExtensionsUnitTests::class, ExtensionsUnitTests::class)
-class UnitTestsSuite
